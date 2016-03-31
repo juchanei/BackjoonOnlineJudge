62afed3b2c6eea658871f58b57df09d8a4687e97
juchanei <juchanei@naver.com>
2016-03-31 19:01:28 +0900

    16.03.31

--- testcase/Source.cpp
+++ testcase/Source.cpp
@@ -5,15 +5,10 @@ using namespace std;
 int main()
 {
 	int h = 1000;
-	int w = 1000;
-	cout << h << " " << w << endl;
+	cout << h << endl;
 	for (size_t i = 0; i < h; i++)
 	{
-		for (size_t j = 0; j < w; j++)
-		{
-			cout << 0;
-		}
-		cout << endl;
+		printf("%d %d\n", rand() % h + 1, rand() % h + 1);
 	}
 
 	return 0;
